bf9b72b053f52bae9f779732babc7b065d5eed46
TanmayeeSathe <sathetanmayee07@gmail.com>
2021-02-16 18:33:38 +0530

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,39 @@
 import { Component } from '@angular/core';
 
+
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [{
+    type: 'success',
+    message: 'This is an success alert',
+  }, {
+    type: 'info',
+    message: 'This is an info alert',
+  }, {
+    type: 'warning',
+    message: 'This is a warning alert',
+  }, {
+    type: 'danger',
+    message: 'This is a danger alert',
+  }, {
+    type: 'primary',
+    message: 'This is a primary alert',
+  }, {
+    type: 'secondary',
+    message: 'This is a secondary alert',
+  }, {
+    type: 'light',
+    message: 'This is a light alert',
+  }, {
+    type: 'dark',
+    message: 'This is a dark alert',
+  }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +41,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'boot';
+
+  alerts: Alert[];
+
+  constructor() {
+    this.reset();
+  }
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  reset() {
+    this.alerts = Array.from(ALERTS);
+  }
+
+
 }
